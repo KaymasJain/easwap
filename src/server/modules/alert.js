dc86aca61281d4d5d30c26901b2c9702979841b6
anshuman73 <dev.anshuman73@gmail.com>
2018-11-29 20:24:19 +0530

    Fix slack messages

--- src/server/modules/alert.js
+++ src/server/modules/alert.js
@@ -5,10 +5,12 @@ module.exports = {
         request.post('https://hooks.slack.com/services/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', {
             json: {
                 text: "New notification from Easwap Server: ",
-                attachments: {
-                    color: status,
-                    text: message
-                }
+                attachments: [
+                    {
+                        color: status,
+                        text: message
+                    }
+                ]
             }
         });
         console.log(`Slack Notification sent !`);
