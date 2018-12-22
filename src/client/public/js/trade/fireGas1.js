6417f80b618288f68f9e6950c3ef4a961ed63e86
Samyak <sjain0410@gmail.com>
2018-12-22 16:17:37 +0530

    gas optimization

--- src/client/public/js/trade/fireGas1.js
+++ src/client/public/js/trade/fireGas1.js
@@ -3,11 +3,7 @@ db.ref('gas').on('value', function (snapshot) {
     if (data.fast < 500) {
         gasLow = data.safeLow / 10;
         gasHigh = data.fast / 10;
-        if ((gasLow + gasHigh) / 2 > data.average / 10) {
-            gasStandard = (gasLow + gasHigh) / 2;
-        } else {
-            gasStandard = data.average / 10;
-        }
+        gasStandard = data.average / 10;
     } else {
         if (data.safeLow < 500) {
             gasLow = data.safeLow / 10;
@@ -15,22 +11,16 @@ db.ref('gas').on('value', function (snapshot) {
             gasLow = 50;
         }
         gasHigh = 50;
-        if (data.average < 500 && ((gasLow + gasHigh) / 2) < 50) {
-            if ((gasLow + gasHigh) / 2 > data.average / 10) {
-                gasStandard = (gasLow + gasHigh) / 2;
-            } else {
-                gasStandard = data.average / 10;
-            }
+        if (data.average < 500) {
+            gasStandard = data.average / 10;
         } else {
             gasStandard = 50;
         }
     }
-    console.log(gasDecide);
     setGas(gasDecide);
 }, function (error) {
     if (error) {
-        console.log('err - 93284' + error);
-        let title = 'ERROR FETCHING DATA';
+        let title = 'UNABLE FETCHING GAS';
         let content = 'Error in fetching gas price from database. Reload and try again if not solve contact us';
         showAlert(title, content);
     }
